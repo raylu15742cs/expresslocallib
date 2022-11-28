55a3f5cdf5abd38a414fd935551dd7022bd239ca
raylu15742cs <raylucs15742@gmail.com>
2022-11-28 09:41:19 -0800

    update cool

--- express-locallibrary-tutorial/routes/cool.js
+++ express-locallibrary-tutorial/routes/cool.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send('your so cool, but');
+  res.send('your so cool, but I am cooler');
 });
 
 module.exports = router;
