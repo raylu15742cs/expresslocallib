3562f357f9e6cca9c9e9aae9b55e8fcab696fbff
raylu15742cs <raylucs15742@gmail.com>
2022-11-28 09:40:56 -0800

    update cool

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -40,4 +40,18 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// Import the mongoose module
+const mongoose = require("mongoose");
+
+// Set up default mongoose connection
+const mongoDB = "mongodb://127.0.0.1/my_database";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+
 module.exports = app;
