26f6e4ca9b58043e178c4e8b00958624b36376d5
raylu15742cs <raylucs15742@gmail.com>
2022-12-01 10:35:22 -0800

    remove cool js

--- express-locallibrary-tutorial/routes/cool.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.send('your so cool, but I am cooler');
-});
-
-module.exports = router;
