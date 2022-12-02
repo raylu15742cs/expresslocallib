9031aeda971598dee7c1e1392f294a33f2177794
raylu15742cs <raylucs15742@gmail.com>
2022-12-01 22:24:30 -0800

    add remove get author controller

--- express-locallibrary-tutorial/controllers/authorController.js
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -120,8 +120,32 @@ exports.author_create_post = [
 
 
 // Display Author delete form on GET.
-exports.author_delete_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author delete GET');
+exports.author_delete_get = (req, res, next) => {
+  async.parallel(
+    {
+      author(callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+      authors_books(callback) {
+        Book.find({ author: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.author == null) {
+        // No results.
+        res.redirect("/catalog/authors");
+      }
+      // Successful, so render.
+      res.render("author_delete", {
+        title: "Delete Author",
+        author: results.author,
+        author_books: results.authors_books,
+      });
+    }
+  );
 };
 
 // Handle Author delete on POST.
