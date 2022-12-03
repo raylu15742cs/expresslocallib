509a656f97b2998c59115ce333850983cd0ec3fc
raylu15742cs <raylucs15742@gmail.com>
2022-12-02 21:04:19 -0800

    update stuff

--- express-locallibrary-tutorial/controllers/authorController.js
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -186,11 +186,67 @@ exports.author_delete_post = (req, res, next) => {
 };
 
 // Display Author update form on GET.
-exports.author_update_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author update GET');
+exports.author_update_get = (req, res, next) => {
+  res.render("author_form", { title: "Update Author" });
 };
 
+
 // Handle Author update on POST.
-exports.author_update_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author update POST');
-};
+exports.author_update_post = [
+  // Validate and sanitize fields.
+  body('first_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('First name must be specified.')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric characters.'),
+  body('family_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Family name must be specified.')
+    .isAlphanumeric()
+    .withMessage('Family name has non-alphanumeric characters.'),
+  body('date_of_birth', 'Invalid date of birth')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body('date_of_death', 'Invalid date of death')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render('author_form', {
+        title: 'Create Author',
+        author: req.body,
+        errors: errors.array(),
+        
+      });
+      return;
+    }
+    // Data from form is valid.
+
+    // Create an Author object with escaped and trimmed data.
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+      _id: req.params.id, //This is required, or a new ID will be assigned!
+    });
+    Author.findByIdAndUpdate(req.params.id, author, {}, (err, theauthor) => {
+      if (err) {
+        return next(err);
+      }
+      // Successful - redirect to new author record.
+      res.redirect(theauthor.url);
+    });
+  },
+];
