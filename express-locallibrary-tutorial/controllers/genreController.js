17cf6a0c00ecca00f1f09d41f31cf4fa9a034e46
raylu15742cs <raylucs15742@gmail.com>
2022-12-02 20:26:07 -0800

    add genre post update

--- express-locallibrary-tutorial/controllers/genreController.js
+++ express-locallibrary-tutorial/controllers/genreController.js
@@ -171,11 +171,52 @@ exports.genre_delete_post = (req, res, next) => {
 }
 
 // Display Genre update form on GET.
-exports.genre_update_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: Genre update GET');
+exports.genre_update_get = (req, res, next) => {
+  res.render("genre_form", { title: "Update Genre" });
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: Genre update POST');
-};
+exports.genre_update_post = [
+  // Validate and sanitize the name field.
+  body('name', 'Genre name required').trim().isLength({ min: 1 }).escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    const genre = new Genre({ name: req.body.name });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render('genre_form', {
+        title: 'Create Genre',
+        genre,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      Genre.findOne({ name: req.body.name }).exec((err, found_genre) => {
+        if (err) {
+          return next(err);
+        }
+
+        if (found_genre) {
+          // Genre exists, redirect to its detail page.
+          res.redirect(found_genre.url);
+        } else {
+          genre.findByIdAndUpdate(req.params.id, genre , {}, (err, thegenre) => {
+            if (err) {
+              return next(err);
+            }
+            // Genre saved. Redirect to genre detail page.
+            res.redirect(thegenre.url);
+          });
+        }
+      });
+    }
+  },
+];
